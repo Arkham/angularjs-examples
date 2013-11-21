f970f430e41d7275cdc423d76064b181118e91cd
Ju Liu <ju.liu@welaika.com>
2013-11-21 10:11:47 +0100

    added smart-float

--- invoice3.js
+++ invoice3.js
@@ -1,5 +1,6 @@
-angular.module('invoice3', ['finance3']).
-  controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
+var app = angular.module('invoice3', ['finance3']);
+
+app.controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
 
   this.qty = 1;
   this.cost = 2;
@@ -14,3 +15,41 @@ angular.module('invoice3', ['finance3']).
     window.alert("Thanks!");
   };
 }]);
+
+var INTEGER_REGEXP = /^\-?\d*$/;
+app.directive('integer', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (INTEGER_REGEXP.test(viewValue)) {
+          // it is valid
+          ctrl.$setValidity('integer', true);
+          return viewValue;
+        } else {
+          // it is invalid, return undefined (no model update)
+          ctrl.$setValidity('integer', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
+
+var FLOAT_REGEXP = /^\-?\d+((\.|\,)\d+)?$/;
+app.directive('smartFloat', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (FLOAT_REGEXP.test(viewValue)) {
+          ctrl.$setValidity('float', true);
+          return parseFloat(viewValue.replace(',', '.'));
+        } else {
+          ctrl.$setValidity('float', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
