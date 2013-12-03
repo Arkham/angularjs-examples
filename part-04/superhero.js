10d90e1b99388bb666762d5e7b711e6a646ee3e9
Ju Liu <ju.liu@welaika.com>
2013-12-03 11:38:46 +0100

    added directive restrictions example

--- part-04/superhero.js
+++ part-04/superhero.js
@@ -3,6 +3,33 @@ var app = angular.module('superhero', []);
 app.directive('superman', function () {
   return {
     restrict: "E",
-    template: "<div>Here I am to save the day</div>"
-  };
-});
+    template: "<div>Here I am Superman to save the day</div>"
+  }
+})
+
+app.directive('wonderwoman', function () {
+  return {
+    restrict: "A",
+    link: function (scope, element) {
+      element.html('Wonder woman from attribute directive')
+    }
+  }
+})
+
+app.directive('hulk', function () {
+  return {
+    restrict: "C",
+    link: function (scope, element) {
+      element.html('Hulk GRR CLASS')
+    }
+  }
+})
+
+app.directive('invisible', function () {
+  return {
+    restrict: "M",
+    link: function () {
+      console.log('I am the invisible man')
+    }
+  }
+})
