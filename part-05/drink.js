1b19c62072fbd97d0e3b5961d9505741c5d368f5
Ju Liu <ju.liu@welaika.com>
2013-12-05 00:15:06 +0100

    added example of @ attribute in directive scope

---
+++ part-05/drink.js
@@ -0,0 +1,14 @@
+var app = angular.module('drinkApp', []);
+
+app.controller('AppCtrl', function ($scope) {
+  $scope.ctrlFlavour = "blackberry";
+})
+
+app.directive('drink', function () {
+  return {
+    scope: {
+      flavor: "@"
+    },
+    template: "<div class='panel'>{{flavor}}</div>"
+  }
+})
