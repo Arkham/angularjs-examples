6ff5637a1b411294f4b9237e6d92d8f0aa8c4caa
Ju Liu <ju.liu@welaika.com>
2013-12-04 12:18:03 +0100

    moved template to html

--- part-04/scope.js
+++ part-04/scope.js
@@ -18,14 +18,6 @@ app.directive('dialog', function () {
       visible: "="
     },
 
-    template:
-    '<div ng-show="visible">' +
-      '<h3>{{title}}</h3>' +
-      '<div class="body" ng-transclude></div>' +
-      '<div class="footer">' +
-        '<button ng-click="onOk()">Save changes</button>' +
-        '<button ng-click="onCancel()">Close</button>' +
-      '</div>' +
-    '</div>'
+    templateUrl: 'dialog.html'
   }
 })
