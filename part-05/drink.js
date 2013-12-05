6b65ffd52507732e6608ccf36ae9b665e8f9d504
Ju Liu <ju.liu@welaika.com>
2013-12-05 18:39:53 +0100

    added example of = attribute in directive scope

--- part-05/drink.js
+++ part-05/drink.js
@@ -12,3 +12,13 @@ app.directive('drink', function () {
     template: "<div class='panel'>{{flavor}}</div>"
   }
 })
+
+app.directive('sweet', function () {
+  return {
+    scope: {
+      flavor: "="
+    },
+    template: "<div class='panel'>{{flavor}}</div>"
+  }
+})
+
