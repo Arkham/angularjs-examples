e7b5612cc468ca9f658b6ff75bd29042d70bdfaf
Ju Liu <ju.liu@welaika.com>
2013-12-26 15:22:54 +0100

    added template cache example

---
+++ part-08/cache.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', []);
+
+app.run(function($templateCache) {
+  $templateCache.put('zippy.html',
+                     '<div> <h3 ng-click="toggleContent()">{{title}}</h3> <div ng-show="isContentVisible" ng-transclude></div> </div>')
+});
+
+app.directive('zippy', function ($templateCache) {
+  console.log($templateCache.get('zippy.html'));
+
+  return {
+    restrict: "E",
+    transclude: true,
+    scope: {
+      title: "@"
+    },
+    template: $templateCache.get('zippy.html'),
+    link: function (scope) {
+      scope.isContentVisible = false;
+
+      scope.toggleContent = function () {
+        scope.isContentVisible = !scope.isContentVisible;
+      }
+    }
+  }
+})
+
