0acf0fa66972fd03ebb2145642b0d945396502eb
Ju Liu <ju.liu@welaika.com>
2013-12-27 01:55:15 +0100

    added more examples of routeProvider API

--- part-09/ng_view.js
+++ part-09/ng_view.js
@@ -1,13 +1,21 @@
 var app = angular.module('app', ['ngRoute'])
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'app.html',
-    controller: 'AppCtrl'
-  }).when('/foo', {
-    templateUrl: 'foo.html',
-    controller: 'FooCtrl'
-  })
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app.html',
+      controller: 'AppCtrl'
+    })
+    .when('/foo', {
+      templateUrl: 'foo.html',
+      controller: 'FooCtrl'
+    })
+    .when('/pizza', {
+      template: "<p>Yummy</p>"
+    })
+    .otherwise({
+      template: "<p>Nothing to see here, move on</p>"
+    })
 })
 
 app.controller('AppCtrl', function ($scope) {
